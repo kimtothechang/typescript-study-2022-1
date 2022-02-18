3b3d13de7dd6f92e1bb25669fda655e87d62b71c
chango <kimtothechang@gmail.com>
2022-02-18 23:01:57 +0900

    :memo: Study

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var name = "Chango", age = 27, gender = "male";
-var sayHi = function (name, age, gender) {
-    console.log("Hello ".concat(name, " u r ").concat(age, " and ").concat(gender));
-};
-// argument가 지정된 갯수와 같아야 컴파일 시켜준다.
-sayHi(name, age);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// parameter뒤에 ?가 올경우 선택적인 것이다.
+// parameter뒤어 :타입을 통해 타입을 지정해 줄 수 있다.
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name} u r ${age} and ${gender}`;
+};
+
+// argument가 지정된 갯수와 같아야 컴파일 시켜준다.
+console.log(sayHi("Chango", 27, "male"));
+
+export {};
