8fc75b1d1361737d720f601be0a65c3bd19c9e2a
chango <kimtothechang@gmail.com>
2022-02-19 22:16:35 +0900

    :memo: Study 'class'

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Chango",
-    age: 27,
-    gender: "male",
-};
+// JS에서도 interface를 활용하려면 class를 통해서 가능하다.
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const chango = new Human("chango", 27, "male");
 // parameter뒤에 ?가 올경우 선택적인 것이다.
 // parameter뒤어 :타입을 통해 타입을 지정해 줄 수 있다.
 const sayHi = (person) => {
     return `Hello ${person.name} u r ${person.age} and ${person.gender}`;
 };
 // argument가 지정된 갯수와 같아야 컴파일 시켜준다.
-console.log(sayHi(person));
+console.log(sayHi(chango));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-// interface는 JS에는 없고 TS에만 있는 기능이다
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// JS에서도 interface를 활용하려면 class를 통해서 가능하다.
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Chango",
-  age: 27,
-  gender: "male",
-};
+const chango = new Human("chango", 27, "male");
 
 // parameter뒤에 ?가 올경우 선택적인 것이다.
 // parameter뒤어 :타입을 통해 타입을 지정해 줄 수 있다.
@@ -18,6 +19,6 @@ const sayHi = (person: Human): string => {
 };
 
 // argument가 지정된 갯수와 같아야 컴파일 시켜준다.
-console.log(sayHi(person));
+console.log(sayHi(chango));
 
 export {};
