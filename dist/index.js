d94e606a8e3a6435bf3bcb54131221d5e6fa4fe4
chango <kimtothechang@gmail.com>
2022-02-19 22:02:11 +0900

    :memo: Study 'interface'

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "Chango",
+    age: 27,
+    gender: "male",
+};
 // parameter뒤에 ?가 올경우 선택적인 것이다.
 // parameter뒤어 :타입을 통해 타입을 지정해 줄 수 있다.
-const sayHi = (name, age, gender) => {
-    return `Hello ${name} u r ${age} and ${gender}`;
+const sayHi = (person) => {
+    return `Hello ${person.name} u r ${person.age} and ${person.gender}`;
 };
 // argument가 지정된 갯수와 같아야 컴파일 시켜준다.
-console.log(sayHi("Chango", 27, "male"));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+// interface는 JS에는 없고 TS에만 있는 기능이다
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Chango",
+  age: 27,
+  gender: "male",
+};
+
 // parameter뒤에 ?가 올경우 선택적인 것이다.
 // parameter뒤어 :타입을 통해 타입을 지정해 줄 수 있다.
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name} u r ${age} and ${gender}`;
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name} u r ${person.age} and ${person.gender}`;
 };
 
 // argument가 지정된 갯수와 같아야 컴파일 시켜준다.
-console.log(sayHi("Chango", 27, "male"));
+console.log(sayHi(person));
 
 export {};
